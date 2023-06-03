c7bbbdd5a31dac77d094cb5d90a577fb667d69ef
BumpVersion Action <bumpversion@github-actions>
2023-06-03 23:40:35 +0000

    Bump version: 0.1.4 → 0.1.5

--- wunderground-pws.groovy
+++ wunderground-pws.groovy
@@ -17,7 +17,7 @@ import java.time.Instant
 @Field static final int DEFAULT_FREQUENCY = 60 * 60
 @Field static final int DEFAULT_RETENTION = 24
 
-public static final String version() { return '0.1.4' }
+public static final String version() { return '0.1.5' }
 
 String averageAtt(String attribute) {
     return "${attribute}Avg"
